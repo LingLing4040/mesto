f744449eafb151f4b9fb75db1cae6850557e2bb9
Nikita <filatovnikita1994@yandex.ru>
2021-08-17 14:38:52 +0300

    Добавить попап

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 let popup = document.querySelector('.popup');
 let popup__container = popup.querySelector('.popup__container');
 let editButton = document.querySelector('.edit-button');
+let closeButton = popup.querySelector('.popup__close-button');
 
 function edit() {
     popup.classList.add('popup_opened');
 }
 
+function close() {
+    popup.classList.remove('popup_opened');
+}
+
 editButton.addEventListener('click', edit);
+
+closeButton.addEventListener('click', close);
