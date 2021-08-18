34340dee5962653d9f5dd5aa125b7433ca097412
Nikita <filatovnikita1994@yandex.ru>
2021-08-18 15:47:36 +0300

    Добавить попап

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 let popup = document.querySelector('.popup');
-let popup__container = popup.querySelector('.popup__container');
+let formElement = popup.querySelector('.popup__container');
 let editButton = document.querySelector('.edit-button');
 let closeButton = popup.querySelector('.popup__close-button');
+let nameInput = formElement.querySelector('.popup__input_name');
+let jobInput = formElement.querySelector('.popup__input_occupation');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__occupation');
+
+function opened() {
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+}
 
 function edit() {
     popup.classList.add('popup_opened');
+    opened();
 }
 
 function close() {
@@ -14,3 +24,18 @@ function close() {
 editButton.addEventListener('click', edit);
 
 closeButton.addEventListener('click', close);
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    close();
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+document.addEventListener('keydown', function (event) {
+    if (event.code == 'Escape') {
+        close();
+    }
+});
